90f224bc167f25396f81929e69446095d9d160da
Marc Lamy <marclamy@Mac-14.lan>
2026-08-11 17:25:08 +0200

    Revert "[verified] chore: document and run Charlot in production"
    This reverts commit e7c4b12a895ebd34ea567480ac02d79ba2b7d0c9.

--- scripts/start-production.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-set -e
-export HOME=/Users/marclamy
-export PATH=/Users/marclamy/.local/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin
-export NODE_ENV=production
-cd "${0:A:h}/.."
-exec node node_modules/next/dist/bin/next start --hostname 127.0.0.1 --port 3000
